5725d1438d6d9caa65ebd7036b9dbabe6689db51
Eraycelik0 <celikeray39gmail.com>
2024-12-06 20:32:58 +0300

    fixed

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const path = require("path");
 
 const app = express();
 
+// Statik Dosyalar
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB Bağlantısı
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+    .catch(err => console.error("MongoDB connection error:", err));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 app.use(flash());
 
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 // View Engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Rotalar
 app.use("/", require("./routes/index"));
